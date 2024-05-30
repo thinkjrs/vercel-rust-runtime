36611eaa1550fd9c12b188f061624b8eb572ae57
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2024-05-30 11:24:50 -0500

    :recycle: add custom vercel url env var usage

--- utils/build-url.ts
+++ utils/build-url.ts
@@ -1,15 +1,13 @@
 // https://github.com/vercel/next.js/discussions/16429#discussioncomment-7379305
 export function getBaseUrl() {
   const custom = process.env.NEXT_PUBLIC_SITE_URL;
-  const vercel = process.env.NEXT_PUBLIC_VERCEL_URL;
-
-  if (custom) {
-    return custom;
-  } else if (vercel) {
-    return vercel;
-  } else {
-    return "http://localhost:3000";
-  }
+  const vercel = process.env.VERCEL_URL;
+  const vercelProd = process.env.VERCEL_PROJECT_PRODUCTION_URL;
+  const isProd = process.env.VERCEL_ENV === "production";
+  if (isProd) return `https://${vercelProd}`;
+  else if (custom) return custom;
+  else if (vercel) return `https://${vercel}`;
+  else return "http://localhost:3000";
 }
 
 export function buildUrl(path: string) {
